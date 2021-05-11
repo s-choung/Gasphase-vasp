4956b11f4ec95afb37646516bc708b01a496d5f0
theSeokhyun <71485567+theSeokhyun@users.noreply.github.com>
2021-05-11 11:25:45 +0900

    Add files via upload

---
+++ CH/run_slurm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20     # Cores per node
+#SBATCH --partition=g3           # Queue Name g3
+##
+#SBATCH --job-name="CHg"
+#SBATCH --time=7-12:30          # Runtime limit: Day-HH:MM
+#SBATCH -o STDOUT.%N.%j.out         # STDOUT, %N : nodename, %j : JobID
+#SBATCH -e STDERR.%N.%j.err         # STDERR, %N : nodename, %j : JobID
+
+
+## HPC ENVIRONMENT DON'T REMOVE THIS PART
+. /etc/profile.d/TMI.sh
+##
+
+mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS  /TGM/Apps/VASP/bin/5.3.5/NORMAL/vasp.5.3.5_31MAR2014_GRP7_NORMAL_VTST.x
